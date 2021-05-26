6f9c82bf76d9517fe0eec589dcaa6f8ad0be10ce
Ryan Su <suxi@oregonstate.edu>
2021-05-25 17:27:14 -0700

    Wrote Second Test

--- test_fizz.py
+++ test_fizz.py
@@ -5,5 +5,8 @@ class TestCase(unittest.TestCase):
     def testOne(self):
         self.assertEqual(fizz.fizzBuzz(1),1)
 
+    def testTwo(self):
+        self.assertEqual(fizz.fizzBuzz(3),"fizz")
+
 if __name__ == '__main__':
     unittest.main()
