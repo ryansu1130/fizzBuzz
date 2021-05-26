838692d4ee0d96d3a33639d0bd57d0fa253b0f75
Ryan Su <suxi@oregonstate.edu>
2021-05-25 17:31:32 -0700

    updated fizzBuzz method

--- fizz.py
+++ fizz.py
@@ -2,6 +2,8 @@ def fizzBuzz(num):
     for i in range(0,100):
         if num == 1:
             return 1
+        if num % 3 == 0 and num % 5 == 0:
+            return "FizzBuzz"
         if num % 3 == 0:
             return "fizz"
         elif num % 5 == 0:
